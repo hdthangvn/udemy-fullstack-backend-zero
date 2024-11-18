22af97f9e686570b6851e887956f1a140d110f68
Hoang Duc Thang <ducthanghoang892004@gmail.com>
2024-11-18 10:17:05 +0400

    init project

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
- require('dotenv').config();
+require('dotenv').config();
 const express = require('express') // commonjs 
 const configViewEngine = require('./config/viewengine');
 const webRoutes = require('./routes/web');
+const connection = require('./config/database');
 // get the client
-const mysql = require('mysql2');
 
 
 const app = express() // tạo express application
 const port =  process.env.PORT || 8888 // init port
-console.log('>>>  chegitck :', process.env.PORT )
+console.log('>>>  chegitck :', process.env.PORT ) 
+// khi nào muốn sử dụng process thì xuất cái thư viện dotenv (dòng 1)
 const hostname =  process.env.HOST_NAME 
+
 
 // config template engine
 configViewEngine(app);
@@ -18,21 +20,17 @@ configViewEngine(app);
 app.use('/', webRoutes);
 
 //test connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307, // default: 3306
-    user: 'root', // default: empty
-    password: '123456',
-    database: 'hoidanit',
-});
 
+
+/*
 connection.query(
     'select * from Users u',
     function (err, results, fields) {
-        console.log(">>> results = ", results);
-        console.log(">>> fields = ", fields);
+        console.log(">>>>>> results = ", results );
+        // console.log(">>> fields = ", fields);
     }
-  );
+);
+*/
 
 //run server trên port đã khởi tạo trước đấy
 //nạp các thông tin khai báo ở trên rồi chạy (ví dụ như nạp routes)
