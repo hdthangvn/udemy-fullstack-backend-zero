e193585c31af32bfda71b0c574168fe7689a3a41
Hoang Duc Thang <ducthanghoang892004@gmail.com>
2024-11-19 13:04:23 +0400

    #38 Chức năng Create User

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -16,7 +16,29 @@ const getHoidanit = (req, res) => {
 
 const postCreateUser = (req, res) => {
     console.log(">>> req.body: ", req.body)
-    res.send('create a new user');
+    let email = req.body.email;
+    let name = req.body.myname;
+    let city = req.body.city;
+    //  let {email, name, city} = req.body;
+    console.log(">>> email =", email, "   >>> name = ", name, "     >>> city = ", city)
+
+    //   let {email, name, city} = req.body;
+
+    //   INSERT INTO Users (email, name, city)
+    //   VALUES ("test", "Hoang Duc Thang", "Nghe an");
+    
+
+    connection.query(
+        `INSERT INTO Users (email, name, city)
+        VALUES (?, ?, ?);`,
+        [email , name , city ],
+        function(err, results) {
+
+            console.log(results);
+
+            res.send(' Created user succeed  !!')
+        }
+    );
 }
 
 module.exports = {
